dc634b90bac4b36a034e9a3bce49055b48e7c641
pkhizhnyakov <pkhizhnyakov@phoenixit.ru>
2022-12-05 14:11:19 +0300

    Часть тестов проходит

--- Solution.py
+++ Solution.py
@@ -2,13 +2,36 @@ from typing import List
 
 
 class Solution:
-    def totalSteps(self, nums: List[int]):
-        steps: int = 0;
+    def __init__(self):
+        pass
 
+    def totalSteps(self, nums: List[int]) -> int:
+        steps: int = 0
 
+        indexes: List[int] = []
+        prev_indexes_len: int = 0
 
+        # i: int = 0
+        while True:
+            prev: int = 0
+            for i in range(0, len(nums)):
+                n: int = nums[i]
 
+                if i == 0:
+                    prev = n
+                else:
+                    for tmp in indexes:
+                        if tmp == i:
+                            break
+                    else:
+                        if prev > n:
+                            indexes.append(i)
+                        prev = n
 
+            if prev_indexes_len < len(indexes):
+                steps += 1
+                prev_indexes_len = len(indexes)
+            else:
+                break
 
-
-        return steps;
+        return steps
--- Starter.py
+++ Starter.py
@@ -1,14 +1,24 @@
 from typing import List
+from Solution import Solution
 
 
-def run_test(list: List[int]):
-    if (len(list) > 100):
-        print("Test array [length=" + len(list) + "]")
+def run_test(list_numbers: List[int]):
+    if len(list_numbers) > 100:
+        print("Test array [length=" + str(len(list_numbers)) + "]")
     else:
-        print("Test array [" + ''.join(list) + "]")
+        s: str = ""
+        for i in range(0, len(list_numbers)):
+            s += ("," if i > 0 else "") + str(list_numbers[i])
+        print("Test array [" + s + "]")
+
+    sol = Solution()
+    res: int = sol.totalSteps(list_numbers)
+    print("*****Result: " + str(res))
 
 
 test1: List[int] = [1, 2, 4, 3]  # 1
 run_test(test1)
 test2: List[int] = [4, 5, 7, 7, 13]  # 0
 run_test(test2)
+test3: List[int] = [5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11]  # 3
+run_test(test3)
