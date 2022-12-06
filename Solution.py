b07a90a3460b9c4d6472b8a2a15b632bdfe632b3
pkhizhnyakov <pkhizhnyakov@phoenixit.ru>
2022-12-06 19:25:40 +0300

    v3: неплохо работает, но медленно

--- Solution.py
+++ Solution.py
@@ -3,13 +3,13 @@ from typing import List
 
 class Solution:
 
-    def next_step(self, all_indexes: set[int], nums: List[int]) -> tuple[set[int], set[int]]:
+    def next_step(self, nums: List[int], used_indexes: set[int]) -> tuple[set[int], set[int]]:
         prev: int = 0
         indexes: set[int] = set()
         indexes_min: set[int] = set()
         all_prev_smaller: bool = True
 
-        for i in all_indexes:
+        for i in used_indexes:
             n: int = nums[i]
 
             if i == 0:
@@ -25,26 +25,17 @@ class Solution:
 
         return indexes, indexes_min
 
-    def trim_nums(self, nums: List[int], all_indexes: set[int]) -> List[int]:
-        result: List[int] = []
-        for i in all_indexes:
-            result.append(nums[i])
-        return result
-
     def totalSteps(self, nums: List[int]) -> int:
         steps: int = 0
 
-        all_indexes: set[int] = set([i for i in range(0, len(nums))])
+        used_indexes: set[int] = set([i for i in range(0, len(nums))])
 
         while True:
-            indexes, indexes_min = self.next_step(all_indexes, nums)
+            indexes, indexes_min = self.next_step(nums, used_indexes)
 
             if len(indexes) > 0:
                 steps += 1
-                # all_indexes -= indexes
-                # all_indexes -= indexes_min
-                nums = self.trim_nums(nums, all_indexes - indexes - indexes_min)
-                all_indexes: set[int] = set([i for i in range(0, len(nums))])
+                used_indexes -= indexes.union(indexes_min)
             else:
                 break
 
