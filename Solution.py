65bf31e0b7752c75fabd2f07094046e3be1c4995
pkhizhnyakov <pkhizhnyakov@phoenixit.ru>
2022-12-07 23:22:48 +0300

    v5: ошибка в подсчете

--- Solution.py
+++ Solution.py
@@ -31,21 +31,32 @@ class Solution:
 
         # 3.Взять последний элемент из предыдущего массива PREV_MAX, и перебором в следующем массиве найти число элементов до PREV_MAX <= CURR_MIN
         # 4.Максимальное число шагов в массивах - это число оставшихся шагов (четыре, а результат 5. Но шаг 2 можно пропустить, тогда сразу будет 5)
+        total_max: int = -1
         prev_max: int = -1
         j_max: int = -1
         for i, tmp in enumerate(lowering_lists):
             if i > 0:
+                plus_steps: int = 0
+
                 for j, n in enumerate(tmp):
-                    if prev_max <= n:
-                        if j_max < (j-1):
-                            j_max = (j-1)
+                    if prev_max <= n and plus_steps == 0:
+                        plus_steps = j
+
+                    if total_max <= n:
+                        new_j_max = (j-1) + plus_steps
+
+                        if j_max < new_j_max:
+                            j_max = new_j_max
                         break
                 else:
-                    if j_max < len(tmp)-1:
-                        j_max = len(tmp)-1
+                    new_j_max = len(tmp)-1 + plus_steps
+                    if j_max < new_j_max:
+                        j_max = new_j_max
 
-            if prev_max < tmp[len(tmp)-1]:
-                prev_max = tmp[len(tmp)-1]
+            if total_max < tmp[len(tmp)-1]:
+                total_max = tmp[len(tmp)-1]
+
+            prev_max = tmp[len(tmp)-1]
 
         steps = j_max + 1
 
